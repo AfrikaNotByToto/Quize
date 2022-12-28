1ef2b3f10cee49fe00c43114f1b5b10fba569081
Anastasia <anastasia-vinogradova5@yandex.ru>
2022-12-29 00:38:17 +0300

    release controller

--- mvc/Controller.js
+++ mvc/Controller.js
@@ -12,12 +12,38 @@ class Controller {
     const model = this.#model;
     // отображаем ту страницу, на которой мы сейчас находимся
     switch (model.getPage()) {
-      case 'start': {
+      case "start": {
         const topic = this.#view.renderSelectTopicPage(model.getTopics());
         model.chooseTopic(topic);
         return this.run();
       }
-      // ...
+      // выбрана тема
+      // getQuestions(), stopGame() - from model
+      case "choose-topic": {
+        const choice = this.#view.renderSelectTopicPage(
+          this.#model.chooseTopic()
+        );
+        if (choice) {
+          this.#model.getQuestions(choice);
+        } else {
+          this.#model.stopGame();
+        }
+        return this.run();
+      }
+      // дан ответ
+      // renderGiveAnswer() - from view
+      // gaveAnswer(), rightAnswer(), wrongAnswer - from model
+      case "gave-answer": {
+        const answer = this.#view.renderGiveAnswer(this.#model.gaveAnswer());
+        if (answer) {
+          this.#model.rightAnswer();
+        } else {
+          this.#model.wrongAnswer();
+        }
+        return this.run();
+      }
+      default:
+        throw new Error("Wrong page");
     }
   }
 }
