ef73e79550c7071ec9826ff383c5a788bd863cac
Tanya <alisaeva4498@mail.ru>
2022-12-29 12:55:50 +0300

    trying async readline

--- mvc/View.js
+++ mvc/View.js
@@ -1,16 +1,35 @@
-const readlineSync = require('readline-sync');
+const fs = require("fs");
+const readline = require("readline-promise").default;
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 class View {
-  renderSelectTopicPage(themes) {
-    // нам пришёл список тем, нужно вывести их на экран
-    // здесь твой код...
+  constructor() {}
 
-    // затем даём пользователю возможность выбрать тему
-    const topic = readlineSync.question('Введите тему: ');
-
-    // и возвращаем контроллеру выбранную тему
+  async showTopics(arr) {
+    const topic = await rl.questionAsync(
+      `\nВыберите категорию!\n\n${arr[0]}\n${arr[1]}\n${arr[2]}\n\n`
+    );
     return topic;
   }
 }
+
+// const readlineSync = require('readline-sync');
+
+// class View {
+//   renderSelectTopicPage(themes) {
+//     // нам пришёл список тем, нужно вывести их на экран
+//     // здесь твой код...
+
+//     // затем даём пользователю возможность выбрать тему
+//     const topic = readlineSync.question('Введите тему: ');
+
+//     // и возвращаем контроллеру выбранную тему
+//     return topic;
+//   }
+// }
 
 module.exports = View;
