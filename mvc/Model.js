130109651fdccf18d911a36193a91da6503c859e
Tanya <alisaeva4498@mail.ru>
2022-12-29 10:45:22 +0300

    trying to choose and get question

--- mvc/Model.js
+++ mvc/Model.js
@@ -1,39 +1,34 @@
 const fs = require('fs').promises;
 
 class Model {
-constructor() {
+  async chooseTopic() {
+    const topic = await fs.readdir('../topics', 'utf-8');
+    console.log(topic.map((el) => el.split('_')[0]));
+  }
 
-}
-
-async chooseTopic () {
-  const topic = await fs.readdir('./topics', 'utf-8');
-  return topic.map((el) => el.split('_')[0]);
-}
-
-chooseTopic()
-
-async chooseQuestion (topic) {
-const question = await fs.readFile(`./topics/${topic}_flashcard_data.txt`, 'utf-8');
-let splitQestion = question.split('\n\n').map((el) => el.split('\n'));
-
-let result = [];
-for(let i = 0; i < splitQestion.length; i += 1) {
-  result.push(splitQestion[i][1])
-}
-return result
-}
-
-
+  async chooseQuestion(topic) {
+    const questions = await fs.readFile(
+      `../topics/${topic}_flashcard_data.txt`,
+      'utf-8'
+    );
+    const splitQuestion = questions.split('\n\n').map((el) => el.split('\n'));
+    const result = [];
+    for (let i = 0; i < splitQuestion.length; i += 1) {
+      result.push({
+        type: 'input',
+        name: `quest${i}`,
+        message: splitQuestion[i][0],
+        answer: splitQuestion[i][1],
+      });
+    }
+    return result;
+  }
   // Сначала приложение находится на стартовой странице выбора темы.
   // Подумай, какие ещё страницы будут в твоём приложении?
   // #page = 'select-topic';
 
   // Подумай какие данные будут нужны View, чтобы рендерить эти страницы.
   // Исходя из этих данных определись какие поля будет содержать модель.
-
-  getPage() {
-    return this.#page;
-  }
 
   // chooseTopic(topic) {
   //   // Тема выбрана, сделай необходимые изменения в модели (в т.ч. измени this.page).
